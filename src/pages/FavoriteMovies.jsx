2ea5f6720fc3a437a2cd1dbd7a78f956367f7ebb
macmar89 <marian.karola@gmail.com>
2021-07-31 16:36:52 +0200

    favorites movies zaciatok

--- src/pages/FavoriteMovies.jsx
+++ src/pages/FavoriteMovies.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { useLocalStorage } from "../global/helpers/useLocalStorage";
+import { IoIosRemoveCircleOutline } from "react-icons/io";
+import { BsInfoCircle } from "react-icons/bs";
+import { NavLink } from "react-router-dom";
 
 const FavoriteMovies = () => {
   const [favorites, setFavorites] = useLocalStorage(
@@ -20,21 +23,42 @@ const FavoriteMovies = () => {
     <div>
       <h2 className="my-2 text-center">Favorites Movies</h2>
 
-      <table>
-        <tr>
-          <th>Title</th>
-          <th></th>
-        </tr>
+      <table className="table">
+        <thead>
+          <tr>
+            <th className="ps-3" colSpan="4">
+              Title
+            </th>
+            <th className="text-center" colSpan="1">
+              More Info
+            </th>
+            <th className="text-center" colSpan="1">
+              Remove
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {favorites.map(movie => (
+            <tr>
+              <th className="ps-3" colSpan="4">
+                {movie.Title}
+              </th>
+              <td className="text-center" colSpan="1">
+                <NavLink to={`/movie/${movie.imdbID}`}>
+                  <BsInfoCircle style={{ color: "green", cursor: "pointer" }} />
+                </NavLink>
+              </td>
+              <td className="text-center" colSpan="1">
+                <IoIosRemoveCircleOutline
+                  className=""
+                  onClick={() => remove(movie.imdbID)}
+                  style={{ color: "red", cursor: "pointer" }}
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
-
-      {favorites.map(movie => (
-        <div className="d-flex">
-          <p>{movie.Title}</p>
-          <span onClick={() => remove(movie.imdbID)} className="ms-3">
-            remove from Favorites
-          </span>
-        </div>
-      ))}
     </div>
   );
 };
