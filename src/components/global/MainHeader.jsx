36edb3504cc4832dae040dfc0781a23dc74c6460
macmar89 <marian.karola@gmail.com>
2021-08-01 00:01:54 +0200

    game over

--- src/components/global/MainHeader.jsx
+++ src/components/global/MainHeader.jsx
@@ -4,11 +4,17 @@ import film from "../../images/film-frame.png";
 
 const MainHeader = ({ title }) => {
   return (
-    <div className="header text-center">
+    <div className="header text-center my-3">
       <div className="d-none d-md-flex container position-relative justify-content-center align-items-center my-5 ">
         <h1>{title}</h1>
         <img
-          className="position-absolute rounded"
+          className="position-absolute "
+          style={{ left: 50 }}
+          src={popcorn}
+          alt="popcorn"
+        />
+        <img
+          className="position-absolute "
           style={{ right: 50 }}
           src={popcorn}
           alt="popcorn"
